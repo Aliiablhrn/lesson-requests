bef510823a20ab5c18657751c901f1c409d92db4
Aliia <aliia@MacBook-Pro-Aliia.local>
2022-06-07 11:39:28 +0600

    added post request

--- main.js
+++ main.js
@@ -10,3 +10,28 @@ GET - получение данных
 ? команды для запуска json-server
 json-server - w db.json -p 8000
 */
+/*
+CRUD - Create Read Update Delete
+Read(Get)
+Update(Put/Patch)
+Delete(Delete)
+*/
+
+const API = "http://localhost:8000/todos";
+
+let inpAdd = document.getElementById('inp-add');
+let btnAdd = document.getElementById('btn-add');
+// console.log(inpAdd, btnAdd);
+btnAdd.addEventListener('click', function(){
+    let newTodo = {
+        todo: inpAdd.value
+    }
+    // console.log(newTodo);
+    fetch(API, {
+        method: "POST",
+        body: JSON.stringify(newTodo),
+        headers: {
+            "Content-type": "application/json; charset=utf-8"
+        }
+    })
+})
